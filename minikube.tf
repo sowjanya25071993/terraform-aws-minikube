121922248a7f425bf69c3390ce626650ab58e151
sowji <jujjurisowjanya@gmail.com>
2024-04-03 11:00:07 +0530

    new

---
+++ minikube.tf
@@ -0,0 +1,15 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "C:\Users\sowjanya\.ssh\id_rsa.pub"
+  aws_subnet_id = "subnet-05d8429e96841af42"
+  hosted_zone = "sowjanyaaws.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+}
