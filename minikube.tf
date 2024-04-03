14b1f86aed384610a1383d884ee0377476f1d058
sowji <jujjurisowjanya@gmail.com>
2024-04-03 11:02:49 +0530

    new

--- minikube.tf
+++ minikube.tf
@@ -4,7 +4,7 @@ module "minikube" {
   aws_region    = "us-east-1"
   cluster_name  = "roboshop"
   aws_instance_type = "t3.medium"
-  ssh_public_key = "~\.ssh\id_rsa.pub"
+  ssh_public_key = "~/.ssh/id_rsa.pub"
   aws_subnet_id = "subnet-05d8429e96841af42"
   hosted_zone = "sowjanyaaws.online"
   hosted_zone_private = false
